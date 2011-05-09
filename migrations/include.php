fd8ef7678797333b0bd88a073325129ee4d75639
yannickmahe <mahe.yannick@gmail.com>
2011-05-09 13:36:45 +0200

    added migration description on readme

--- migrations/include.php
+++ migrations/include.php
@@ -1,11 +1,11 @@
 <?php
 
-$db_host = 'localhost';
-$db_name = 'hanzi';
-$db_user = 'root';
-$db_password = '';
+$mysql_db_host = 'localhost';
+$mysql_db_name = 'hanzi';
+$mysql_db_user = 'root';
+$mysql_db_password = '';
 
-$mysqli = new mysqli($db_host, $db_user, $db_password, $db_name);
+$mysqli = new mysqli($mysql_db_host, $mysql_db_user, $mysql_db_password, $mysql_db_name);
 
 $master_name = 'hanzi';
 
